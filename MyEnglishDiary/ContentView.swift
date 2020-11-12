1025fa16ed9e6c803c352adbfe92d8b537b20b34
Anton Skvortsov <silmion@gmail.com>
2020-11-12 18:50:04 +0200

    Initial Commit

---
+++ MyEnglishDiary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyEnglishDiary
+//
+//  Created by Антон Скворцов on 12.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyEnglishDiary/MyEnglishDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyEnglishDiaryApp.swift
+//  MyEnglishDiary
+//
+//  Created by Антон Скворцов on 12.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct MyEnglishDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
