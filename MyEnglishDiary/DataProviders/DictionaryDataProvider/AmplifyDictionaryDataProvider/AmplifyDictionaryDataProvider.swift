55b9376a851e273e48dc106575662cae81e089d7
Anton Skvortsov <silmion@gmail.com>
2021-12-26 23:46:15 +0600

    Updated GraphQL scheme

--- MyEnglishDiary/DataProviders/DictionaryDataProvider/AmplifyDictionaryDataProvider/AmplifyDictionaryDataProvider.swift
+++ MyEnglishDiary/DataProviders/DictionaryDataProvider/AmplifyDictionaryDataProvider/AmplifyDictionaryDataProvider.swift
@@ -11,11 +11,15 @@ import Amplify
 final class AmplifyDictionaryDataProvider: DictionaryDataProvider {
     func search(for text: String, _ completion: @escaping (Result<DictionarySearchResult, Error>) -> Void) {
         let lexemeModelKeys = LexemeGraphQLModel.keys
-        Amplify.DataStore.query(LexemeGraphQLModel.self, where: lexemeModelKeys.text.beginsWith(text)) { result in
-                switch result {
-                case .success(let models): completion(.success(models.mapModels()))
-                case .failure(let dataStoreError): print(dataStoreError)
-                }
+        Amplify.DataStore.query(
+            LexemeGraphQLModel.self,
+            where: lexemeModelKeys.text.beginsWith(text),
+            paginate: QueryPaginationInput.page(0, limit: 20)
+        ) { result in
+            switch result {
+            case .success(let models): completion(.success(models.mapModels()))
+            case .failure(let dataStoreError): print(dataStoreError)
             }
+        }
     }
 }
--- amplify/generated/models/AmplifyModels.swift
+++ amplify/generated/models/AmplifyModels.swift
@@ -5,7 +5,7 @@ import Foundation
 // Contains the set of classes that conforms to the `Model` protocol. 
 
 final public class AmplifyModels: AmplifyModelRegistration {
-  public let version: String = "d1e534247cb431e5bb2513af937a0541"
+  public let version: String = "3a402704f268c622ff81474c0b54144d"
   
   public func registerModels(registry: ModelRegistry.Type) {
     ModelRegistry.register(modelType: LexemeGraphQLModel.self)
